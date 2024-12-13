9e8051511938588d722993ed23b60a9396198ee3
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-13 16:18:10 -0300

    Fix/ Adjust guessing logic to use one unit or less

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -9,9 +9,11 @@ public class GuessGame {
         System.out.println("Adivinhe o numero que foi sorteado de 1 a 10: ");
         Random random = new Random();
         int anynumber = random.nextInt(10);
+
+
+        System.out.println(anynumber);
         Scanner scn1 = new Scanner(System.in);
         int guessNum = scn1.nextInt();
-
         numberDraw(anynumber, guessNum);
     }
     public static void numberDraw(int anynumber, int guessNum){
@@ -20,12 +22,14 @@ public class GuessGame {
             System.out.println("Parabéns você acertou e ganhou 10 pontos!\n" +
                     "Quer jogar outra rodada ? ");
             System.out.println("================== **** ==================");
-        }else if (guessNum > anynumber){
-            System.out.println("O número digitado é maior que o numero sorteado, " +
+        }else if (guessNum == anynumber +1){
+            System.out.println("O número digitado é uma unidade acima do numero sorteado, " +
                     "você ganhou 5 pontos! ");
-        }else if (guessNum < anynumber) {
-            System.out.println("O número digitado é menor que o numero sorteado, " +
+        }else if (guessNum == anynumber -1) {
+            System.out.println("O número digitado é uma unidade abaixo do numero sorteado " +
                     "você ganhou 5 pontos! ");
+        }else {
+            System.out.println("Você não ganhou nenhum ponto!");
         }
 
     }
