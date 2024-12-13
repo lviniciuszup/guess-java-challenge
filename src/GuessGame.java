316a6922f45480157cb3c701477c292e7a28cc80
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-13 14:59:07 -0300

    Add condition when the guessed number is higher

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class GuessGame {
     public static void main(String[] args) {
         System.out.println("================== **** ==================");
-        System.out.println("Jogo do Adivinha");
+        System.out.println("Bem-vindo ao Jogo do Adivinha");
         System.out.println("================== **** ==================");
         System.out.println("Adivinhe o numero que foi sorteado de 1 a 10: ");
         Random random = new Random();
@@ -13,9 +13,15 @@ public class GuessGame {
         int guessNum = scn1.nextInt();
 
     }
-    public static void numberDraw(int anynumber, int guessNUm){
-        if (anynumber == guessNUm){
-            System.out.println("Parabéns você acertou!");
+    public static void numberDraw(int anynumber, int guessNum){
+        if (anynumber == guessNum){
+            System.out.println("================== **** ==================");
+            System.out.println("Parabéns você acertou e ganhou 10 pontos!\n" +
+                    "Quer jogar outra rodada ? ");
+            System.out.println("================== **** ==================");
+        }else if (guessNum > anynumber){
+            System.out.println("O número digitado é maior que o numero sorteado, " +
+                    "você ganhou 5 pontos! ");
         }
 
     }
