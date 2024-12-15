d87aa3b9ab62dbefb20b1e0c77afc86f02be8149
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-15 18:56:51 -0300

    feat/Game loop and score calculation

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -3,46 +3,66 @@ import java.util.Scanner;
 
 public class GuessGame {
     public static void main(String[] args) {
-        System.out.println("================== **** ==================");
-        System.out.println("Bem-vindo ao Jogo do Adivinha");
-        System.out.println("================== **** ==================");
-        System.out.println("Escolha uma dificuldade :");
-        System.out.println("1- Fácil 2- Médio 3 - Difícil ");
+        Scanner scanner = new Scanner(System.in);
+        boolean keepPlaying = true;
+        int score = 0;
+        while(keepPlaying){
+            System.out.println("================== **** ==================");
+            System.out.println("Bem-vindo ao Jogo do Adivinha");
+            System.out.println("================== **** ==================");
+            System.out.println("Escolha uma dificuldade :");
+            System.out.println("1- Fácil 2- Médio 3 - Difícil ");
 
-        Random random = new Random();
-        int anyEasy = random.nextInt(10);
-        int anyMed = random.nextInt(50);
-        int anyHard = random.nextInt(100);
+            Random random = new Random();
+            int anyEasy = random.nextInt(10);// Gera um número de 0 a 9 para o nivel facil
+            int anyMed = random.nextInt(50);// Gera um número de 0 a 50 para o nivel medio
+            int anyHard = random.nextInt(100);// Gera um número de 0 a 100 para o nivel dificil
 
-        Scanner scn1 = new Scanner(System.in);
-        int difficulty = scn1.nextInt();
 
-        numberDraw(anyEasy, anyMed, anyHard, difficulty);
+            int difficulty = scanner.nextInt();
+
+            // Soma a pontuação retornada pelo metodo numberDraw
+            score += numberDraw(anyEasy, anyMed, anyHard, difficulty);
+
+            System.out.println("Sua pontuação atual é: " + score);
+            System.out.println("Deseja jogar novamente?" +
+                    "\n 1 - Sim \n 2 - Não");
+            int playAgain = scanner.nextInt();
+            if (playAgain == 2){ keepPlaying = false;
+                System.out.println("Obrigado por jogar!");
+                System.out.println("Sua pontuação final foi: " + score);
+
+            }
+        }
     }
-
-    public static void numberDraw(int anyEasy, int anyMed, int anyHard, int difficulty) {
+    // Realiza o sorteio de um número com base na dificuldade escolhida e soma os pontos
+    public static int numberDraw(int anyEasy, int anyMed, int anyHard, int difficulty) {
+        int points = 0;
         switch (difficulty) {
             case 1:
-                System.out.println("Você escolheu o modo fácil!" +
+                System.out.println("Você escolheu o modo Fácil!" +
                         " Escolha um numero para ser sorteado de 1 a 10");
                 Scanner scnEasy = new Scanner(System.in);
-                int guessNum = scnEasy.nextInt();
-                if (anyEasy == guessNum) {
+                int guessNumEasy = scnEasy.nextInt();
+                // Verifica se o palpite está correto ou próximo
+                if (anyMed == guessNumEasy) {
                     System.out.println("================== **** ==================");
-                    System.out.println("Parabéns você acertou e ganhou 10 pontos!\n" +
-                            "Quer jogar outra rodada ? ");
+                    System.out.println("Parabéns você acertou e ganhou 10 pontos!");
+                    points += 10;
                     System.out.println("================== **** ==================");
-                } else if (anyEasy == guessNum + 1) {
+                } else if (anyMed == guessNumEasy + 1) {
                     System.out.println("O número digitado é uma unidade acima do numero sorteado, " +
                             "você ganhou 5 pontos! ");
-                } else if (anyEasy == guessNum - 1) {
+                    points += 5;
+                } else if (anyMed == guessNumEasy - 1) {
                     System.out.println("O número digitado é uma unidade abaixo do numero sorteado " +
                             "você ganhou 5 pontos! ");
+                    points += 5;
                 } else {
                     System.out.println("Você não ganhou nenhum ponto!");
                 }
                 break;
-            case 2 :
+            case 2:
                 System.out.println("Você escolheu o modo Médio!" +
                         " Escolha um numero para ser sorteado de 1 a 50");
                 Scanner scnMed = new Scanner(System.in);
@@ -51,18 +71,21 @@ public class GuessGame {
                     System.out.println("================== **** ==================");
                     System.out.println("Parabéns você acertou e ganhou 10 pontos!\n" +
                             "Quer jogar outra rodada ? ");
+                    points += 10;
                     System.out.println("================== **** ==================");
                 } else if (anyMed == guessNumMed + 1) {
                     System.out.println("O número digitado é uma unidade acima do numero sorteado, " +
                             "você ganhou 5 pontos! ");
+                    points += 5;
                 } else if (anyMed == guessNumMed - 1) {
                     System.out.println("O número digitado é uma unidade abaixo do numero sorteado " +
                             "você ganhou 5 pontos! ");
+                    points += 5;
                 } else {
                     System.out.println("Você não ganhou nenhum ponto!");
                 }
                 break;
-            case 3 :
+            case 3:
                 System.out.println("Você escolheu o modo Difícil!" +
                         " Escolha um numero para ser sorteado de 1 a 100");
                 Scanner scnHard = new Scanner(System.in);
@@ -71,17 +94,21 @@ public class GuessGame {
                     System.out.println("================== **** ==================");
                     System.out.println("Parabéns você acertou e ganhou 10 pontos!\n" +
                             "Quer jogar outra rodada ? ");
+                    points += 10;
                     System.out.println("================== **** ==================");
                 } else if (anyHard == guessNumHard + 1) {
                     System.out.println("O número digitado é uma unidade acima do numero sorteado, " +
                             "você ganhou 5 pontos! ");
+                    points += 5;
                 } else if (anyHard == guessNumHard - 1) {
                     System.out.println("O número digitado é uma unidade abaixo do numero sorteado " +
                             "você ganhou 5 pontos! ");
+                    points += 5;
                 } else {
                     System.out.println("Você não ganhou nenhum ponto!");
                 }
                 break;
         }
+        return points;
     }
 }
